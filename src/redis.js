fc1c25a44abbe69b422418b081207ea96b9d7a80
Below Bed <jordanmeta4677@gmail.com>
2022-05-22 21:34:43 +0800

    update redis

--- src/redis.js
+++ src/redis.js
@@ -1,10 +1,6 @@
 import Redis from 'ioredis'
 
-let client = null
+const client = new Redis("rediss://:6b6e58c4236243a196a06b7b5e2ddeca@apn1-amazed-tuna-33092.upstash.io:33092");
 export default function () {
-  if (client) {
-    return client
-  }
-  client = new Redis("rediss://:6b6e58c4236243a196a06b7b5e2ddeca@apn1-amazed-tuna-33092.upstash.io:33092");
   return client
 }
