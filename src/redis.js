b24fb14c0bfb9f39087335a61f9e19ef87552521
Below Bed <jordanmeta4677@gmail.com>
2022-05-22 22:17:25 +0800

    up redis

--- src/redis.js
+++ src/redis.js
@@ -1,6 +1,10 @@
-import Redis from 'ioredis'
+import { createClient } from 'redis';
 
-const client = new Redis("rediss://:6b6e58c4236243a196a06b7b5e2ddeca@apn1-amazed-tuna-33092.upstash.io:33092");
+let client = null
 export default function () {
+  if (client) {
+    return client
+  }
+  client = createClient({ url: "rediss://:6b6e58c4236243a196a06b7b5e2ddeca@apn1-amazed-tuna-33092.upstash.io:33092" });
   return client
 }
