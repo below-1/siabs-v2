1a3c24c950f72b2c561412b919f2200faa71a454
Below Bed <jordanmeta4677@gmail.com>
2022-05-24 20:56:47 +0800

    savepoint

---
+++ src/routes/app/jadwal/[id]/shift/shifts/_empty.svelte
@@ -0,0 +1,8 @@
+<script>
+  import FButton from '$lib/fbutton.svelte'
+</script>
+
+<div class="w-full py-24 bg-gray-200 rounded border flex flex-col gap-y-4 items-center justify-center">
+  <h1 class="text-gray-600 text-3xl font-black">Belum Ada Shift pada jadwal ini</h1>
+  <FButton primary>tambah shift</FButton>
+</div>
---
+++ src/routes/app/jadwal/[id]/shift/shifts/index.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Empty from './_empty.svelte'
+</script>
+
+<div class="container px-4 py-12">
+  <Empty />
+</div>
