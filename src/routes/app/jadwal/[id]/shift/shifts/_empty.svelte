918c44ba8dc8aebf7d172db501c5434008bbb3e5
Below Bed <jordanmeta4677@gmail.com>
2022-05-24 21:22:09 +0800

    shift creation

--- src/routes/app/jadwal/[id]/shift/shifts/_empty.svelte
+++ src/routes/app/jadwal/[id]/shift/shifts/_empty.svelte
@@ -1,8 +1,11 @@
 <script>
+  import { getContext } from 'svelte'
   import FButton from '$lib/fbutton.svelte'
+
+  const item = getContext('item')
 </script>
 
 <div class="w-full py-24 bg-gray-200 rounded border flex flex-col gap-y-4 items-center justify-center">
   <h1 class="text-gray-600 text-3xl font-black">Belum Ada Shift pada jadwal ini</h1>
-  <FButton primary>tambah shift</FButton>
+  <FButton primary path={`/app/jadwal/${item.jadwal.id}/shift/shifts/create`}>tambah shift</FButton>
 </div>
