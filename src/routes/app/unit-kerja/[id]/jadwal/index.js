72027bd8844b9b0cbb8008f12a0edb4255842327
Below Bed <jordanmeta4677@gmail.com>
2022-05-27 22:59:48 +0800

    unit kerja jadwal

--- src/routes/app/unit-kerja/[id]/jadwal/index.js
+++ src/routes/app/unit-kerja/[id]/jadwal/index.js
@@ -1,8 +1,48 @@
-import db from '../../../../../db'
+import db from '../../../../../db';
+import day from '$lib/day';
 
 export async function get(event) {
   const id = event.params.id
+  let start = event.url.searchParams.get('start');
+  console.log(`start = ${start}`)
+  start = start ? new Date(start) : day().startOf('month').toDate();
+  let end = event.url.searchParams.get('end');
+  console.log(`end = ${end}`)
+  end = end ? new Date(end) : day(start).endOf('month').toDate();
+
+  console.log(`id = ${id}`)
+  console.log(`start = ${start}`)
+  console.log(`end = ${end}`)
+
   const sql = db()
+
+  const aggregation = await sql`
+    with 
+      days as (
+        select generate_series(
+          ${start},
+          ${end},
+          '1 day'::interval
+        )::date as d
+      ),
+    foo as (
+      select 
+        a.alert_masuk::date as d, count(a.id)
+        from jadwal j
+            join shift s on j.id = s.id_jadwal
+              join absen a on a.id_shift = s.id
+          where 
+            id_unit_kerja = '4bed80d5-3224-44e0-b5dd-71c77b60a43e'
+            and alert_masuk >= ${start}
+            and alert_keluar <= ${end}
+          group by a.alert_masuk
+      )
+      select days.d date, coalesce(foo.count, 0) as total_absen 
+        from days left join foo on days.d = foo.d
+  `
+  console.log(aggregation);
+  console.log('aggregation');
+
   const items = await sql`
     select 
         j.id,
@@ -19,7 +59,8 @@ export async function get(event) {
   `
   return {
     body: {
-      items
+      items,
+      aggregation
     }
   }
 }
--- src/routes/app/unit-kerja/[id]/jadwal/index.svelte
+++ src/routes/app/unit-kerja/[id]/jadwal/index.svelte
@@ -1,21 +1,73 @@
 <script>
   import { getContext } from 'svelte';
-  import PageHeader from '$lib/page-header.svelte'
-  import ListItem from './_list-item.svelte'
+  import { browser } from '$app/env';
+  import PageHeader from '$lib/page-header.svelte';
+  import ListItem from './_list-item.svelte';
+  import MonthYearSelect from '$lib/month-year-select.svelte';
+  import { client_fetch_json } from '$lib/http';
+  import day from '$lib/day';
 
   const unitKerja = getContext('unitKerja');
+  const d = day();
 
   export let items = [];
+  export let aggregation = [];
+
+  let loading = false;
+  let year = d.year();
+  let month = d.month();
+
+  function getDateInterval(year, month) {
+    const start = new Date(year, month, 1);
+    const end = day(start).endOf('month').toDate();
+    return {
+      start: start.toISOString(),
+      end: end.toISOString()
+    }
+  }
+  $: dateInterval = getDateInterval(year, month);
+
+  async function loadAggregation(dateInterval) {
+    if (!browser) {
+      return;
+    }
+    const response = await client_fetch_json({
+      method: 'GET',
+      path: `/app/unit-kerja/${unitKerja.id}/jadwal`,
+      params: dateInterval
+    })
+    aggregation = response.aggregation;
+  }
+  $: loadAggregation(dateInterval);
+
 </script>
 
-<PageHeader>
-  <h1 class="text-2xl font-bold">Jadwal {unitKerja.nama}</h1>
-</PageHeader>
-
-<div class="container px-4 py-6">
-  {#each items as item}
-    <ListItem
-      item={item}
+<div class="container py-6">
+  <div class="flex flex-wrap justify-center md:justify-between items-center gap-x-4 outer-padding mb-6">
+    <h1 class="text-xl font-bold">Daftar Jadwal</h1>
+    <MonthYearSelect 
+      bind:year={year}
+      bind:month={month}
     />
-  {/each}
+  </div>
+
+  <div class="flex flex-col md:px-4 border-t">
+    {#each aggregation as dateGroup}
+      <div class="flex border-b hover:bg-gray-200">
+
+        <div class="bg-gray-100 p-2 flex flex-col items-center justify-center w-20">
+          <div class="text-lg font-bold">{day(dateGroup.date).format('DD')}</div>
+          <div class="text-xs font-bold">{day(dateGroup.date).format('dddd')}</div>
+        </div>
+
+        <div class="bg-gray-100 p-2 px-4 flex flex-col items-center justify-center border-l">
+          <div class="text-gray-600">{day(dateGroup.date).format('DD-MM-YYYY')}</div>
+        </div>
+
+        <div class="flex-grow flex flex-wrap items-center justify-end gap-x-4 pr-4 md:pr-0">
+          <span class="font-bold">{dateGroup.total_absen} pegawai</span>
+        </div>
+      </div>
+    {/each}
+  </div>
 </div>
