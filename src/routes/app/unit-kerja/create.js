6af09be3e3f078a9171a0b817924c9eab58d52ab
Below Bed <jordanmeta4677@gmail.com>
2022-06-04 23:43:53 +0800

    fix unit kerja creation

--- src/routes/app/unit-kerja/create.js
+++ src/routes/app/unit-kerja/create.js
@@ -4,20 +4,19 @@ import { upload_image } from '../../../upload'
 export async function post(event) {
   const data = await event.request.formData();
   const session = event.locals.session
-  const tenant = session.tenant
   let payload = {
     nama: data.get('nama'),
     alamat: data.get('alamat'),
     latitude: data.get('latitude'),
     longitude: data.get('longitude'),
-    id_tenant: tenant.id,
     tipe: data.get('tipe')
   }
 
   const avatar_file = data.get('avatar')
-  console.log(avatar_file)
-  const avatar = await upload_image(avatar_file)
-  payload.avatar = avatar
+  if (avatar_file) {
+    const avatar = await upload_image(avatar_file)
+    payload.avatar = avatar
+  }
 
   const sql = db()
   const [unit_kerja] = await sql`
