da0dd4e3f7c1978e7ed06c4a7100e3b7fe5281fb
Below Bed <jordanmeta4677@gmail.com>
2022-06-04 19:50:01 +0800

    remove relativeTime

--- src/lib/day.js
+++ src/lib/day.js
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
+// import relativeTime from 'dayjs/plugin/relativeTime'
 
 import 'dayjs/locale/id'
 
-dayjs.extend(relativeTime)
+// dayjs.extend(relativeTime)
 dayjs.locale('id')
 
 export default dayjs
