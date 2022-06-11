345dd1ac7e2b7348da6d39dcc0d7ad43fde34232
Below Bed <jordanmeta4677@gmail.com>
2022-06-11 16:45:55 +0800

    fix close-expired

--- src/routes/api/close-expired.js
+++ src/routes/api/close-expired.js
@@ -10,11 +10,15 @@ export async function get(event) {
       set status_masuk = 'alpa'
       where  
         (alert_masuk + '90 minute'::interval) < ${now}
-        and tipe != 'dl'
   `;
   await sql`
     update absen
-      set status_keluar = 'alpa'
+      set status_keluar = 
+        (case 
+          when status_masuk is null then 'alpa'
+          when status_masuk = 'alpa' then 'alpa'
+          else 'late'
+        end)
       where  
         (alert_keluar + '90 minute'::interval) < ${now}
         and tipe != 'dl'
